935529412677d94ae14ccf26c6a326b618a9210b
manganuioanadiana-code <manganuioanadiana@gmail.com>
2026-09-06 23:28:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -92,7 +92,8 @@ st.markdown("""
 @keyframes sali { 0% {transform: translateY(20px); opacity:0;} 50% {opacity:1;} 100% {transform: translateY(-20px); opacity:0;} }
 </style>
 """, unsafe_allow_html=True)
-col_sx, col_centro, col_dx = st.columns([1, 2.2, 1])
+col_sx, col_centro, col_dx = st.columns([1, 1, 1])
+st.markdown("<style>div[data-testid='column']{margin-top:-180px;}</style>", unsafe_allow_html=True)
 
 with col_sx:
     st.markdown("""<div style="background-color:#FFECCF; padding:20px; border-radius:15px;"><h3>Marchi trattati</h3>• Candy<br><br>• Indesit<br><br>• LG<br><br>• Smeg<br><br>• Whirlpool<br><br>• Beko</div>""", unsafe_allow_html=True)
