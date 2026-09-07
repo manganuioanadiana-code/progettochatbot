804c09d80da1de3912ca1b25f7039d48ea378914
manganuioanadiana-code <manganuioanadiana@gmail.com>
2026-09-07 18:25:59 +0200

    Fix page icon configuration in Streamlit app

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from langchain_core.output_parsers import StrOutputParser
 # Elenco di tutte le icone Streamlit:
 # https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
 st.set_page_config(page_title= "Consulente virtuale",
-                   page_icon=":material/local_laundry_service:"
+                   page_icon=":material/local_laundry_service:",
                    layout="wide")
 # Personalizzazione colori:
 # Personalizzazione colori:
