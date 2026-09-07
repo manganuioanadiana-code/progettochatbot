b186c1fe412b7ed4cdd15e9070a3d1a36904a8e7
manganuioanadiana-code <manganuioanadiana@gmail.com>
2026-09-07 18:20:10 +0200

    Fix formatting in app.py for page config

--- app.py
+++ app.py
@@ -20,8 +20,8 @@ from langchain_core.output_parsers import StrOutputParser
 # Elenco di tutte le icone Streamlit:
 # https://streamlit-emoji-shortcodes-streamlit-app-gwckff.streamlit.app/
 st.set_page_config(page_title= "Consulente virtuale",
-                   page_icon=":material/local_laundry_service:")
-                   layout="wide"
+                   page_icon=":material/local_laundry_service:"
+                   layout="wide")
 # Personalizzazione colori:
 # Personalizzazione colori:
 st.markdown("""
